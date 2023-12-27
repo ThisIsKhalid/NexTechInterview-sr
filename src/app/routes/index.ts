72947a0a7a7dcb7a48569970dfccaf4699e2c560
ThisIsKhalid <iamkhalid.hs@gmail.com>
2023-12-27 19:52:32 +0600

    project setup

---
+++ src/app/routes/index.ts
@@ -0,0 +1,45 @@
+import express from 'express';
+import { AuthRoutes } from '../modules/auth/auth.route';
+import { ServiceRoutes } from '../modules/service/service.route';
+import { UserRoutes } from '../modules/user/user.route';
+import { BookingRoutes } from '../modules/bookings/booking.route';
+import { CourseReviewRoutes } from '../modules/serviceReview/serviceReview.route';
+import { OrderRoutes } from '../modules/paymentOrder/order.route';
+import { TutorRoute } from '../modules/tutors/tutor.route';
+
+const router = express.Router();
+
+const moduleRoutes = [
+  // ... routes
+  {
+    path: '/auth',
+    routes: AuthRoutes,
+  },
+  {
+    path: '/users',
+    routes: UserRoutes,
+  },
+  {
+    path: '/services',
+    routes: ServiceRoutes,
+  },
+  {
+    path: '/bookings',
+    routes: BookingRoutes,
+  },
+  {
+    path: '/reviews',
+    routes: CourseReviewRoutes,
+  },
+  {
+    path: '/orders',
+    routes: OrderRoutes,
+  },
+  {
+    path: '/tutors',
+    routes: TutorRoute,
+  },
+];
+
+moduleRoutes.forEach(route => router.use(route.path, route.routes));
+export default router;
